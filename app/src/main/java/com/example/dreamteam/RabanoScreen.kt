f31627d0bc75e2adbd409845ac0d93a47bcd2fc1
Eligsir <eligsircodes@gmail.com>
2026-01-18 20:23:47 +0800

    Image size changes

--- app/src/main/java/com/example/dreamteam/RabanoScreen.kt
+++ app/src/main/java/com/example/dreamteam/RabanoScreen.kt
@@ -6,19 +6,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.*
 import com.example.dreamteam.ui.theme.DreamTeamTheme
 
