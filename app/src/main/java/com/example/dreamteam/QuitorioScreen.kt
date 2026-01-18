a54a62e042d819852522de244537052911d98032
Quitorio <marcalvinq@gmail.com>
2026-01-18 20:47:49 +0800

    Added bio for Quitorio

--- app/src/main/java/com/example/dreamteam/QuitorioScreen.kt
+++ app/src/main/java/com/example/dreamteam/QuitorioScreen.kt
@@ -1,9 +1,32 @@
 package com.example.dreamteam
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dreamteam.ui.theme.DreamTeamTheme
 
 class QuitorioScreen : ComponentActivity() {
@@ -12,8 +35,44 @@ class QuitorioScreen : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DreamTeamTheme {
-
+                QuitorioProfile(this)
             }
         }
     }
 }
+
+@Composable
+fun QuitorioProfile(context: Context) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = { TopBar() }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                ProfileImage(Modifier
+                    .height(400.dp),
+                    painterResource(R.drawable.quitorio)
+                )
+
+                NameDisplay("Marc Alvin P. Quitorio")
+
+                BioDisplay(
+                    "I am a 21 year old 3rd Year Bachelor of Science in Information Technology student and I like to read and cook."
+                )
+
+                NavigationButton("Go Back", {
+                    val mainIntent = Intent(context, MainActivity::class.java)
+                    context.startActivity(mainIntent)
+                })
+            }
+        }
+    }
+}
+
